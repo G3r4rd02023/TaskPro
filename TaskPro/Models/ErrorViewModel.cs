c142fc610803d20964461e57664602e823bbab0d
Gerardo Lanza <glanza007@gmail.com>
2024-09-05 09:27:58 -0600

    Creacion del proyecto

---
+++ TaskPro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
